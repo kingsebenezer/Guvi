8115c80a080cbdf288251a8758dde7d814e3ced6
kingsebenezer <40835127+kingsebenezer@users.noreply.github.com>
2018-07-14 16:09:51 +0530

    Update LeapYear.c

--- LeapYear.c
+++ LeapYear.c
@@ -5,8 +5,8 @@ int year;
 printf("Enter a Year:");
 scanf("%d",&year);
 if(year%4==0)
-  printf("%d is Leap year",year);
+  printf("yes");
 else
-  printf("%d is Not a Leap year",year);
+  printf("no");
  
 }
