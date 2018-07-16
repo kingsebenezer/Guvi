c2664cfbaea9085a9643182d89e470038227cfb7
kingsebenezer <40835127+kingsebenezer@users.noreply.github.com>
2018-07-16 21:37:07 +0530

    Add files via upload

---
+++ Alpa.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+    printf("Enter an alphabet: ");
+    scanf("%c",&c);
+
+    if( isalpha(c) )
+  
+        printf("Alphabet");
+    else
+        printf("NO");
+    return 0;
+    
+    	
+    	
+    	
+}
