ad31940098fcb72ce318cdba5e0bf0bad25c0c06
kingsebenezer <40835127+kingsebenezer@users.noreply.github.com>
2018-07-18 20:53:54 +0530

    Add files via upload

---
+++ Five_Multiples.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n,i;
+    printf("Enter Number: ");
+    scanf("%d",&n);
+
+    for(i=1;i<6;i++)
+    {
+    	
+    	printf("%d \t",i*n);
+	}
+    
+  	
+}
