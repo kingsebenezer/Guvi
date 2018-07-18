5c96404e09b986edbb9d9f65e6ca4ecb7a37c182
kingsebenezer <40835127+kingsebenezer@users.noreply.github.com>
2018-07-18 20:36:28 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int p,q,z;
+    printf("Enter an Number & Power: ");
+    scanf("%d %d",&p,&q);
+
+    z= pow(p,q);
+    printf("%d",z);
+  	
+}
