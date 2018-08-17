9194b3b4c9ba36d80d1df9ac24115c1f0787d2d5
kingsebenezer <40835127+kingsebenezer@users.noreply.github.com>
2018-08-17 13:36:44 +0530

    Update rev_rem_vow.c

--- rev_rem_vow.c
+++ rev_rem_vow.c
@@ -9,12 +9,10 @@ void main()
     l=strlen(word);
     for(i=l-1;word[i]!='\0';i--)
     {
-        if(word[i]!='a'&&word[i]!='e'&&word[i]!='i'&&word[i]!='o'&&word[i]!='u'&&word[i]!='A'&&word[i]!='E'&&word[i]!='I'&&word[i]!='O'&&word[i]!='U')
-        {
-        printf("%c",word[i]);
-        }
-        
-        
-    }
-    
+     if(word[i]!='a'&&word[i]!='e'&&word[i]!='i'&&word[i]!='o'&&word[i]!='u'&&word[i]!='A'&&word[i]!='E'&&word[i]!='I'&&word[i]!='O'&&word[i]!='U')
+     {
+     printf("%c",word[i]);
+     }
 }
+}
+
